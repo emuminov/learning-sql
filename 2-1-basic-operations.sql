bf19731b923122a172794430b3692cf0ef8825a6
emuminov <jseldardw@gmail.com>
2025-11-04 02:36:03 +0100

    feat: added more basoc operations to 2-1-basic-operations.sql style: formatted 2-1-basic-operations.sql with pgformatter

--- 2-1-basic-operations.sql
+++ 2-1-basic-operations.sql
@@ -1,35 +1,93 @@
 /* TABLE CREATION */
-CREATE TABLE person
-  (person_id SMALLINT UNSIGNED, /* smallint stores 2-bit number */
-  fname VARCHAR(20), /* stores up to 65,535 bytes */
-  lname VARCHAR(20),
-  eye_color ENUM('BR', 'BL', 'GR'), /* enum have possible values BR, BL or GR */
-  birth_date DATE, /* stores YYYY-MM-DD */
-  street VARCHAR(30),
-  city VARCHAR(20),
-  state VARCHAR(20),
-  country VARCHAR(20),
-  postal_code VARCHAR(20),
-  CONSTRAINT pk_person PRIMARY KEY (person_id)
+CREATE TABLE person (
+    person_id smallint UNSIGNED,
+    /* smallint stores 2-bit number */
+    fname varchar(20),
+    /* stores up to 65,535 bytes */
+    lname varchar(20),
+    eye_color ENUM ('BR', 'BL', 'GR'),
+    /* enum have possible values BR, BL or GR */
+    birth_date date,
+    /* stores YYYY-MM-DD */
+    street varchar(30),
+    city varchar(20),
+    state varchar(20),
+    country varchar(20),
+    postal_code varchar(20),
+    CONSTRAINT pk_person PRIMARY KEY (person_id)
 );
+
 
 /* reverse action */
 DROP TABLE person;
 
+
 /* CREATE TABLE WITH FOREIGN KEY */
-CREATE TABLE favorite_food
-  (person_id SMALLINT UNSIGNED,
-  food VARCHAR(20),
-  CONSTRAINT pk_favorite_food PRIMARY KEY (person_id, food),
-  CONSTRAINT fk_fav_food_person_id FOREIGN KEY (person_id)
-  REFERENCES person (person_id)
+CREATE TABLE favorite_food (
+    person_id smallint UNSIGNED,
+    food varchar(20),
+    CONSTRAINT pk_favorite_food PRIMARY KEY (person_id, food),
+    CONSTRAINT fk_fav_food_person_id FOREIGN KEY (person_id) REFERENCES person (person_id)
 );
+
 
 /* reverse action */
 DROP TABLE favorite_food;
 
+
 /* TABLE ALTERATION */
 /* enabling of the auto-incrementing of the key */
-ALTER TABLE person MODIFY person_id SMALLINT UNSIGNED AUTO_INCREMENT;
+ALTER TABLE person MODIFY person_id smallint UNSIGNED AUTO_INCREMENT;
+
 
 /* DATA INSERTION */
+INSERT INTO person (fname, lname, eye_color, birth_date)
+    VALUES ('William', 'Turner', 'BR', '1972-08-11');
+
+INSERT INTO favorite_food (person_id, food)
+    VALUES (1, 'ice cream'),
+    (1, 'pizza'),
+    (1, 'hamburger');
+
+
+/* DATA DELETION */
+/* reverse action */
+DELETE FROM favorite_food
+WHERE person_id = 1;
+
+
+/* without where clause all the data from the table is deleted */
+DELETE FROM favorite_food;
+
+
+/* SELECTION WITH ORDERING */
+SELECT
+    *
+FROM
+    favorite_food
+WHERE
+    person_id = 1
+ORDER BY
+    food;
+
+
+/* DATA UPDATING */
+UPDATE
+    person
+SET
+    street = '1225 Tremont St.',
+    city = 'Boston',
+    state = 'MA'
+WHERE
+    person_id = 1;
+
+
+/* reverse action */
+UPDATE
+    person
+SET
+    street = NULL,
+    city = NULL,
+    state = NULL
+WHERE
+    person_id = 1;
